c83bd45dd958cf08e21fccc3c97b32d251f5c8ca
Richard Hudson <richard.hudson@standardbank.co.za>
2024-07-20 13:30:49 +0200

    added stream

--- java/src/main/java/com/dvt/hackathon/demo/controller/TransactionController.java
+++ java/src/main/java/com/dvt/hackathon/demo/controller/TransactionController.java
@@ -1,29 +1,32 @@
 package com.dvt.hackathon.demo.controller;
 
-import com.dvt.hackathon.demo.model.Transaction;
+import com.dvt.hackathon.demo.api.Transaction;
 import com.investec.hackathon.demo.api.GetTransactions;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping
+@RequiredArgsConstructor
 public class TransactionController {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
-    private TransactionMappingService transactionMappingService;
+    private final RestTemplate restTemplate;
 
     @GetMapping("/transactions")
     public List<Transaction> getTransactions() {
         String apiURL = "https://plankton-app-zkjow.ondigitalocean.app/za/pb/v1/accounts/4675778129910189600000003/transactions";
-        var response = restTemplate.getForEntity(apiURL, GetTransactions.class).getBody().getData();
-        return new List.of(new Transaction().setDescription());
+        var transactions = restTemplate.getForEntity(apiURL, GetTransactions.class).getBody().getData().getTransactions();
+        return transactions.stream().map(transaction -> {
+            final var transactionDto = new Transaction();
+            transactionDto.setDescription(transaction.getDescription());
+            transactionDto.setAmount(transaction.getAmount());
+            return transactionDto;
+        }).collect(Collectors.toList());
     }
 }

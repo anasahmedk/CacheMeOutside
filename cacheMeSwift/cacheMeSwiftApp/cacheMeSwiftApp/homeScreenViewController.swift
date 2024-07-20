4e5095eeafb2b3806f6b35f8eb81b21739303b4a
sebastianj7 <sebastianjacobs2000@gmail.com>
2024-07-20 13:22:24 +0200

    home screen

---
+++ cacheMeSwift/cacheMeSwiftApp/cacheMeSwiftApp/homeScreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  homeScreenViewController.swift
+//  cacheMeSwiftApp
+//
+//  Created by Sebastian Jacobs on 2024/07/20.
+//
+
+import UIKit
+
+class homeScreenViewController: UIViewController {
+
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var memberOne: UIButton!
+    @IBOutlet weak var memberTwo: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        homeButton.layer.cornerRadius = 50
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

3e58645d2134d2c78a341600489a147690249f27
anasahmedk <159020871+anasahmedk@users.noreply.github.com>
2024-07-22 09:49:13 +0200

    Update ChatGPTService.java

--- java/src/main/java/com/dvt/hackathon/demo/service/ChatGPTService.java
+++ java/src/main/java/com/dvt/hackathon/demo/service/ChatGPTService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class ChatGPTService {
 
     private static final String API_URL = "https://api.openai.com/v1/chat/completions";
-    private static final String API_KEY = "sk-svcacct-ueXNZLLklFRJgiPZQeOwT3BlbkFJ0fGfYLTteJMeWI45GLWu";
+    private static final String API_KEY = "secret";
     private static final String MODEL = "gpt-3.5-turbo";
 
     public String chatGPT(String message) throws IOException {
